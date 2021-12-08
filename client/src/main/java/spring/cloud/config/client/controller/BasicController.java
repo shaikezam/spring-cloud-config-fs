2fbd579262479186ed20bcce34a6aea356d11dc0
Shai Zambrovski <SZambrovski@illusivenetworks.com>
2021-12-08 11:12:14 +0200

    fs automatic

--- client/src/main/java/spring/cloud/config/client/controller/BasicController.java
+++ client/src/main/java/spring/cloud/config/client/controller/BasicController.java
@@ -1,12 +1,9 @@
 package spring.cloud.config.client.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.config.environment.Environment;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
--- server/src/main/java/spring/cloud/config/server/ServerApplication.java
+++ server/src/main/java/spring/cloud/config/server/ServerApplication.java
@@ -1,15 +1,33 @@
 package spring.cloud.config.server;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.bus.event.RefreshRemoteApplicationEvent;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.UUID;
+
+@Configuration
+@EnableAutoConfiguration
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
+	}
+
+	@EventListener
+	public void onApplicationEvent(RefreshRemoteApplicationEvent event) {
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.postForObject("http://localhost:50001/actuator/busrefresh", new Object(), String.class);
+		System.out.println(event);
 	}
 
 }
